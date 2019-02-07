af37d34a3643e3e77bfe911f7071507199de37b8
Michael Abrahamsen <mail@michaelabrahamsen.com>
2019-02-06 22:45:47 -0700

    map urls outside of wsgi_app

--- index.py
+++ index.py
@@ -1,15 +1,20 @@
 from Silo.routing.router import Router
 from routes import url_map
+from werkzeug.routing import Map
 
-class App: 
+
+urls = Map(url_map)
+
+class App:
     def wsgi_app(self, environ, start_response):
-        response = Router(url_map).dispatch_request(environ)
+        response = Router(urls).dispatch_request(environ)
         return response(environ, start_response)
 
     def __call__(self, environ, start_response):
         return self.wsgi_app(environ, start_response)
 
+
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     app = App()
-    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
+    run_simple('127.0.0.1', 5050, app, use_debugger=True, use_reloader=True)
